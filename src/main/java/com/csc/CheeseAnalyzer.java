92b92a32e2867607caab203ffcd63efff6bf841d
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-10-08 14:04:37 -0700

    Created Buffered Reader object
    Created Buffered Reader object

--- src/main/java/com/csc/CheeseAnalyzer.java
+++ src/main/java/com/csc/CheeseAnalyzer.java
@@ -1,5 +1,23 @@
 package com.csc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CheeseAnalyzer {
   // You can put source code here
+    public static void main(String[] args) {
+      
+
+
+      try {
+        BufferedReader reader = new BufferedReader(new FileReader("cheese_data.csv"));
+        System.out.println(reader.readLine());
+        
+      } catch (IOException e) {
+        // TODO: handle exception
+        System.out.println("Could not read the file.");
+      }
+      
+    }
 }
